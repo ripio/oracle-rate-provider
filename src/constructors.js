963414a66562f344469f4b17ffeb1c84199ee512
Nicolas <menendeznm@gmail.com>
2019-09-16 12:18:13 -0300

    add network condition in constructors

--- src/constructors.js
+++ src/constructors.js
@@ -2,20 +2,40 @@ const W3 = require('web3');
 
 const env = require('../environment.js');
 
-module.exports.w3 = new W3(new W3.providers.HttpProvider(env.node));
+
+module.exports.getw3 = () => {
+  const network = process.env.NETWORK;
+  console.log(network);
+  if (network == 'mainnet') {
+    // TO DO - CHANGE TO MAINNET 
+    return new W3(new W3.providers.HttpProvider(env.nodeRopsten));
+  }
+  if (network == 'ropsten') {
+    return new W3(new W3.providers.HttpProvider(env.nodeRopsten));
+  }
+};
+
+module.exports.w3 = this.getw3();
+
 
 module.exports.instanceOracleFactory = async () => {
-  return new this.w3.eth.Contract(env.oracleFactory.abi, env.oracleFactory.address);
+  const network = process.env.NETWORK;
+  if (network == 'mainnet') {
+    return new this.w3.eth.Contract(env.oracleFactory.abi, env.oracleFactory.address);
+  }
+  if (network == 'ropsten') {
+    return new this.w3.eth.Contract(env.oracleFactoryTest.abi, env.oracleFactoryTest.address);
+  }
 };
 
 module.exports.instanceOracles = async (oracleFactory) => {
   const oracles = [];
 
-  const symbols = env.oracles;
+  const symbols = process.env.NETWORK == 'mainnet' ? env.oracles : env.oraclesTest;
 
   console.log('All oracles:');
-
   for (const symbol of symbols) {
+
     const oracleAddr = await oracleFactory.methods.symbolToOracle(symbol).call();
     if (oracleAddr === '0x0000000000000000000000000000000000000000') {
       console.log('\tCurrency: ' + symbol + ', the oracle dont exists');
@@ -36,7 +56,7 @@ module.exports.instanceSigners = async (pk) => {
   if (this.w3.utils.isHexStrict(pk)) {
     signer = this.w3.eth.accounts.privateKeyToAccount(pk);
     this.w3.eth.accounts.wallet.add(signer);
-    signer.data = env.signersData;
+    signer.data = process.env.NETWORK == 'mainnet' ? env.signersData : env.signersDataTest;
   } else {
     console.log('The private key its not valid: ' + pk);
   }
