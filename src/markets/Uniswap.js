69b9e3020d1514814f12aedfa5cda3f0e74f9516
Nicolas <menendeznm@gmail.com>
2019-10-08 12:28:48 -0300

    update uniswap market

--- src/markets/Uniswap.js
+++ src/markets/Uniswap.js
@@ -1,88 +1,71 @@
 const Market = require('./Market.js');
 const UniswapFactory = require('../contracts/UniswapFactory.js');
+const UniswapExchange = require('../contracts/UniswapExchange.js');
+const OracleFactory = require('../contracts/OracleFactory.js');
 const constants = require('../../environment/constants.js');
+const logger = require('../logger.js');
+const utils = require('../utils.js');
 
 module.exports = class Uniswap extends Market {
   constructor(w3, options) {
     super(w3);
     this.w3 = w3;
     this.options = options;
-    this.factory = new w3.eth.Contract(UniswapFactory.abi, options.uniswapFactoryAddress);
+    this.oracleFactory = new w3.eth.Contract(OracleFactory.abi, options.oracleFactoryContract);
+    this.factory = new w3.eth.Contract(UniswapFactory.abi, options.uniswapFactoryContract);
     this.WEI = this.bn('1000000000000000000');
   }
 
   async init() {
     const base = constants[this.options.network].baseToken;
-    this.rcnExchange.address = await this.factory.methods.getExchange(base).call();
+    const rcnExchangeAddress = await this.factory.methods.getExchange(base).call();
+    this.rcnExchange = new this.w3.eth.Contract(UniswapExchange.abi, rcnExchangeAddress);
     return this;
   }
 
   async getRate(currency_from, currency_to, decimals) {
     let rate;
-    if (process.env.NETWORK == 'mainnet') {
-      // TODO remove this when use in mainnet
-      // change to mainnet netEnv
-      // const w3 = new W3(new W3.providers.HttpProvider('https://mainnet.infura.io/v3/f6427a6723594cdd8affb596d357d268'));
-      // this.rcnExchange = new w3.eth.Contract(netEnv.markets.uniswap.exchangeABI, '0xD91FF16Ef92568fC27F466C3c5613e43313Ab1dc');
-      // TODO_END
 
-      if (currency_to === 'ETH') {
-        const amountInRCN = this.bn('1000');// TODO think this amount
-        const amountInWEI = this.WEI.mul(amountInRCN);
+    const amountInRCN = this.bn('1000');// TODO think this amount
+    const amountInWEI = this.WEI.mul(amountInRCN);
 
-        let rateTo = await this.rcnExchange.methods.getEthToTokenOutputPrice(amountInWEI.toString()).call();
-        rateTo = this.bn(rateTo);
-        let rateFrom = await this.rcnExchange.methods.getTokenToEthInputPrice(amountInWEI.toString()).call();
-        rateFrom = this.bn(rateFrom);
-        // The average of rateFrom and ratoTo RCN
-        rate = rateTo.add(rateFrom).div(this.bn(2)).div(amountInRCN);
-      } else {
-        // TODO integrate other currencies(ERC20 tokensp)
+    let rateTo = await this.rcnExchange.methods.getEthToTokenOutputPrice(amountInWEI.toString()).call();
+    rateTo = this.bn(rateTo);
+    let rateFrom = await this.rcnExchange.methods.getTokenToEthInputPrice(amountInWEI.toString()).call();
+    rateFrom = this.bn(rateFrom);
+    // The average of rateFrom and ratoTo RCN
+    const rateEth = rateTo.add(rateFrom).div(this.bn(2)).div(amountInRCN);
+
+    if (currency_to === 'ETH') {
+      return rateEth.toString();
+    } else {
+    
+      // Get currency_to TEST/DEST TokenSwap rate
+      const destAddress = await this.oracleFactory.methods.symbolToOracle(currency_to).call();
+      if (destAddress === utils.address0x) {
+        logger.info('Currency: ' + currency_to + ', the oracle dont exists');
       }
-      return rate.toString();
+
+      const destExchangeAddress = await this.factory.methods.getExchange(destAddress).call();
+      if (destExchangeAddress === utils.address0x) {
+        logger.info('Exchange: ' + currency_to + ', dont exists');
+        return;
+      } 
+      
+      const destExchange = new this.w3.eth.Contract(UniswapExchange.abi, destExchangeAddress);
+      const amountInDest = this.bn('1000');// TODO think this amount
+      const amountInWEI = this.WEI.mul(amountInDest);
+
+      let rateToDest = await destExchange.methods.getEthToTokenOutputPrice(amountInWEI.toString()).call();
+      const rateTo = this.bn(rateToDest);
+
+      let rateFromDest = await destExchange.methods.getTokenToEthInputPrice(amountInWEI.toString()).call();
+      const rateFrom = this.bn(rateFromDest);
+
+      const rateDest = rateTo.add(rateFrom).div(this.bn(2)).div(amountInDest);
+      rate = this.bn(rateEth).mul(this.bn(10 ** decimals)).div(this.bn(rateDest));
+
     }
-
-    if (process.env.NETWORK == 'ropsten') {
-      const rcnExchange = await new w3.eth.Contract(netEnv.markets.uniswap.exchangeABI, this.rcnExchange.address);
-
-      // console.log(rcnExchange);
-      const amountInRCN = this.bn('1000');// TODO think this amount
-      const amountInWEI = this.WEI.mul(amountInRCN);
-
-      let rateTo = await rcnExchange.methods.getEthToTokenOutputPrice(amountInWEI.toString()).call();
-      rateTo = this.bn(rateTo);
-
-      let rateFrom = await rcnExchange.methods.getTokenToEthInputPrice(amountInWEI.toString()).call();
-      rateFrom = this.bn(rateFrom);
-
-      const rateTest = rateTo.add(rateFrom).div(this.bn(2)).div(amountInRCN);
-
-      if (currency_to === 'ETH') {
-        rate = rateTest;
-      } else {
-        // Get currency_to TEST/DEST TokenSwap rate
-
-        const w3 = new W3(new W3.providers.HttpProvider(netEnv.node));
-        const destExchangeAddress = await this.factory.methods.getExchange(netEnv.destToken).call();
-        const destExchange = await new w3.eth.Contract(netEnv.markets.uniswap.exchangeABI, destExchangeAddress);
-
-        const amountInDest = this.bn('1000');// TODO think this amount
-        const amountInWEI = this.WEI.mul(amountInDest);
-
-        let rateToDest = await destExchange.methods.getEthToTokenOutputPrice(amountInWEI.toString()).call();
-        rateTo = this.bn(rateToDest);
-
-        let rateFromDest = await destExchange.methods.getTokenToEthInputPrice(amountInWEI.toString()).call();
-        rateFrom = this.bn(rateFromDest);
-
-        const rateDest = rateTo.add(rateFrom).div(this.bn(2)).div(amountInDest);
-        rate = this.bn(rateTest).mul(this.bn(10 ** decimals)).div(this.bn(rateDest));
-
-      }
-      return rate.toString();
-    }
-
-    return 'Incorrect network';
-
+    return rate.toString();
   }
 };
