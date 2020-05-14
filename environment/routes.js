12ddf557a4df72da61d8a10042fd9c540a0dfedc
Nicolas Menendez <menendeznm@gmail.com>
2020-05-14 20:04:02 +0200

    update environment routes. Add BTC/USDC pair

--- environment/routes.js
+++ environment/routes.js
@@ -3,25 +3,31 @@ module.exports = {
     {
       currency_from: 'RCN',
       currency_to: 'ETH',
-      exchangesIds: ['binance', 'uniswap', 'huobipro', 'hitbtc'],
+      exchangesIds: ['binance', 'uniswap', 'hitbtc'],
       decimals: 18
     },
     {
       currency_from: 'RCN',
       currency_to: 'BTC',
-      exchangesIds: ['binance', 'huobipro', 'bittrex', 'upbit', 'hitbtc'],
+      exchangesIds: ['binance', 'binance' ,'huobipro', 'bittrex', 'hitbtc'],
       decimals: 18
     },
     {
       currency_from: 'BTC',
       currency_to: 'USD',
-      exchangesIds: ['bittrex', 'kraken', 'gemini', 'bitfinex', 'binance', 'coinbase', 'bitflyer', 'bitstamp', 'poloniex'],
+      exchangesIds: ['bittrex', 'kraken', 'gemini', 'bitfinex', 'binance', 'coinbase', 'bitstamp', 'poloniex'],
       decimals: 2
     },
     {
       currency_from: 'BTC',
+      currency_to: 'USDC',
+      exchangesIds: ['binance', 'poloniex'],
+      decimals: 6
+    },
+    {
+      currency_from: 'BTC',
       currency_to: 'ARS',
-      exchangesIds: ['ripio', 'satoshiTango', 'bitex', 'coinMonitor'],
+      exchangesIds: ['ripio', 'satoshiTango', 'bitex'],
       decimals: 2
     },
     {
